47f9cb8c696b409dba8a37b2cdc91922b9e438be
Unknown <thomas.verbeek@hotmail.com>
2018-04-10 13:52:29 +0200

    1 auto
    1 auto kan rijden

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Controller {
+
+
+
+
+    @FXML
+    private Rectangle car;
+
+    @FXML
+    private void handleButtonAction() {
+        final ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        ses.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                car.setX(car.getX()+4.0);
+            }
+        }, 0, 10, TimeUnit.MILLISECONDS);
+    }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,26 @@
 package sample;
 
+import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1800, 300));
         primaryStage.show();
+
     }
 
 
